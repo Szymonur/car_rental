bddff205147a40af1ba087f3b4332b78954b8675
Szymon Urban <szymonur88828@gmail.com>
2024-11-02 10:32:30 +0100

    init project

---
+++ car_rental/car_rental/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  car_rental
+//
+//  Created by Szymon Urban on 02/11/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
