8f61143151941eb7173dee79172560b76502c34e
Szymon Urban <szymonur88828@gmail.com>
2025-01-25 09:40:44 +0100

    remove .vscode and build from git

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/szymon/Documents/car_rental_api/backend
-# Build directory: /Users/szymon/Documents/car_rental_api/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
